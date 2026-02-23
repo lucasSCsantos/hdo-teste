cfbdc4875c60c32708f3a2ec576c71eab065a6b2
lucasSCsantos <luc.cristovam10@gmail.com>
2026-02-23 19:21:20 -0300

    chore: update ESLint and Prettier configurations

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,6 +1,7 @@
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
 
+
 export default [
   ...baseConfig,
   ...nx.configs['flat/angular'],
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,27 @@
 import nx from '@nx/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import js from '@eslint/js';
 
 export default [
+  js.configs.recommended,
+
   ...nx.configs['flat/base'],
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
+
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+
   {
     ignores: ['**/dist', '**/out-tsc'],
   },
+
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
@@ -24,19 +39,5 @@ export default [
         },
       ],
     },
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.cts',
-      '**/*.mts',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {},
   },
 ];
